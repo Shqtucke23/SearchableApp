5baf1d38d94f31e68ec65276248c6ee19c80c9bc
Shawn Tucker <shqtucke@gmail.com>
2024-06-09 18:24:26 -0400

    Initial Commit

---
+++ GasApp/GasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasAppApp.swift
+//  GasApp
+//
+//  Created by Shawn Tucker on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
